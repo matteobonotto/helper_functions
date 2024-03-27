89eb34c46f8c1387ebf5145de488d7b48a3d7081
matteobonotto <matteob.90@hotmail.it>
2024-03-27 18:36:32 +0100

    fixing bugs

--- helper_functions/general.py
+++ helper_functions/general.py
@@ -29,9 +29,9 @@ def timer_func(func):
 
     """
     def wrapper(*args, **kwargs):
-        t1 = time.time()
+        t1 = time()
         result = func(*args, **kwargs)
-        t2 = time.time()
+        t2 = time()
         print(f'{func.__name__}() executed in {(t2-t1):.6f}s')
         return result
     return wrapper
